0aee0dcf9fcb23be3810d67d807b2f7aedf35709
Francisco Camerini <83650761+FranciscoCamerini@users.noreply.github.com>
2022-04-25 08:41:52 -0300

    Create length_of_last_word.py

---
+++ python/length_of_last_word.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        total = []
+        x = []
+        final = []
+        
+        for i in s:
+            if i != ' ':
+                x.append(i)
+            else:
+                total.append(x)
+                x = []
+                        
+        if x:
+            total.append(x)
+
+        total.reverse()
+
+        for i in total:
+            if i:
+                return len(i)
+    
