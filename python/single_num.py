169f760663ef649bfe73fddb30b5d984019062b7
Francisco Camerini <83650761+FranciscoCamerini@users.noreply.github.com>
2022-04-27 19:22:28 -0300

    Update single_num.py

--- python/single_num.py
+++ python/single_num.py
@@ -1,10 +1,3 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        x = []
-        for num in nums:
-            if num not in x:
-                x.append(num)
-                nums.remove(num)
-        for i in x:
-            if i not in nums:
-                return i
+        return reduce(lambda total, el: total ^ el, nums)
